0dced3d03ced93695a7f881a196a606f8b36d52f
uu04418 <uu04418@163.com>
2019-04-23 10:17:02 +0800

    修理了BUG

--- cloudProvideDept8001/src/main/java/com/provide/dept8001/ProvideDept8001.java
+++ cloudProvideDept8001/src/main/java/com/provide/dept8001/ProvideDept8001.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient // 服务注入注册中心
+@EnableEurekaClient // 服务注入注册中心 1111
 public class ProvideDept8001 {
 	
 	public static void main(String[] args) {
